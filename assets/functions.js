4a3b0e503d17374c2848966ba6712d89578affd8
Nathan Blake <developer@blake-ink.com>
2021-02-21 18:20:09 -0800

    factor out functions for clearner codebase

--- assets/functions.js
+++ assets/functions.js
@@ -13,7 +13,7 @@ const processFetchWord = function(_word){
 			} else {
 				window.word = _word;
 				console.log(dictionaryEntry);
-				//startBtn.style.display = "block";
+				startBtn.style.display = "block";
 				addDictionaryEntryToPage()
 				for(var i = 0; i < _word.length; i++){
 					let letterElement = document.createElement("span");
@@ -52,3 +52,42 @@ const fetchDefinition = function(word){
   	return data;
   });
 } 
+
+// Create the html elements for the dictionary entry and 
+// add it to the page.
+function addDictionaryEntryToPage(){
+	const definitionsDiv = document.querySelector("#definitions-div");
+	definitionsDiv.innerHTML = "";
+	if(dictionaryEntry.pronunciation){
+		const pronunciationTitle = document.createElement("h3");
+		pronunciationTitle.innerHTML = "Pronunciation";
+		definitionsDiv.appendChild(pronunciationTitle);
+		const pronunciation = document.createElement("p");
+		pronunciation.innerHTML = dictionaryEntry.pronunciation;
+		definitionsDiv.appendChild(pronunciation);
+	}
+	if(dictionaryEntry.definitions.length > 0){
+		const definitionsTitle = document.createElement("h3");
+		(dictionaryEntry.definitions.length > 1) ? definitionsTitle.innerHTML = "Definitions" : definitionsTitle.innerHTML = "Definition";
+		definitionsDiv.appendChild(definitionsTitle);
+		const ul = document.createElement("ul");
+		for(var i = 0; i < dictionaryEntry.definitions.length; i++){
+			const li = document.createElement("li");
+			const h4 = document.createElement("h4");
+			h4.innerHTML = dictionaryEntry.definitions[i].type;
+			li.appendChild(h4);
+			const pDefinition = document.createElement("p");
+			pDefinition.classList.add("definition");
+			pDefinition.innerHTML = dictionaryEntry.definitions[i].definition;
+			li.appendChild(pDefinition);
+			if(dictionaryEntry.definitions[i].example){
+				const pExample = document.createElement("p");
+				pExample.classList.add("example");
+				pExample.innerHTML = dictionaryEntry.definitions[i].example;
+				li.appendChild(pExample);
+			}
+			ul.appendChild(li);
+		}
+		definitionsDiv.appendChild(ul);
+	}
+}
