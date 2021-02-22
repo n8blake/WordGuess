5b962bada9cc63ac2f5cc9da47feef131a076a7d
Nathan Blake <developer@blake-ink.com>
2021-02-21 16:30:48 -0800

    add addDictionaryEntryToPage function

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,14 @@ var fetchWord = fetch(wordURL);
 
 var word = "";
 var dictionaryEntry = {};
+var gameRunning = false;
 
 // process the fetched word
 const processFetchWord = function(_word){
 	return _word.then(response => response.json())
 	.then(data => {
 		_word = data[0];
+		//_word = "bushwhacker";
 		//console.log(window.word);
 		fetchDefinition(_word).then((value) => {
 			//console.log(value);
@@ -19,6 +21,8 @@ const processFetchWord = function(_word){
 			} else {
 				window.word = _word;
 				console.log(dictionaryEntry);
+				//startBtn.style.display = "block";
+				addDictionaryEntryToPage()
 				for(var i = 0; i < _word.length; i++){
 					let letterElement = document.createElement("span");
 					//letterElement.innerHTML = word.charAt(i);
@@ -57,13 +61,51 @@ const fetchDefinition = function(word){
   });
 } 
 
+function addDictionaryEntryToPage(){
+	const definitionsDiv = document.querySelector("#definitions-div");
+	definitionsDiv.innerHTML = "";
+	if(dictionaryEntry.pronunciation){
+		const pronunciationTitle = document.createElement("h3");
+		pronunciationTitle.innerHTML = "Pronunciation";
+		definitionsDiv.appendChild(pronunciationTitle);
+		const pronunciation = document.createElement("p");
+		pronunciation.innerHTML = dictionaryEntry.pronunciation;
+		definitionsDiv.appendChild(pronunciation);
+	}
+	if(dictionaryEntry.definitions.length > 0){
+		const definitionsTitle = document.createElement("h3");
+		(dictionaryEntry.definitions.length > 1) ? definitionsTitle.innerHTML = "Definitions" : definitionsTitle.innerHTML = "Definition";
+		definitionsDiv.appendChild(definitionsTitle);
+		const ul = document.createElement("ul");
+		for(var i = 0; i < dictionaryEntry.definitions.length; i++){
+			const li = document.createElement("li");
+			const h4 = document.createElement("h4");
+			h4.innerHTML = dictionaryEntry.definitions[i].type;
+			li.appendChild(h4);
+			const pDefinition = document.createElement("p");
+			pDefinition.classList.add("definition");
+			pDefinition.innerHTML = dictionaryEntry.definitions[i].definition;
+			li.appendChild(pDefinition);
+			if(dictionaryEntry.definitions[i].example){
+				const pExample = document.createElement("p");
+				pExample.classList.add("example");
+				pExample.innerHTML = dictionaryEntry.definitions[i].example;
+				li.appendChild(pExample);
+			}
+			ul.appendChild(li);
+		}
+		definitionsDiv.appendChild(ul);
+	}
+}
+
+
 fetchWord.then(processFetchWord(fetchWord));
 
 var guessedLetters = [];
 const guessedLettersElement = document.querySelector(".guessed-letters");
 
 function guessLetter(event){
-	if(word.length > 0){
+	if(word.length > 0 && gameRunning){
 		if(isLetter(event.key)){
 			const letter = event.key.toLowerCase();
 			console.log(letter);
