49d6f8e7ad9f9926f32b40ad64024e59e8cf22fc
Nathan Blake <developer@blake-ink.com>
2021-02-21 18:19:39 -0800

    add red pulse animation for already guessed letters

--- assets/script.js
+++ assets/script.js
@@ -6,100 +6,7 @@ var word = "";
 var dictionaryEntry = {};
 var gameRunning = false;
 
-// process the fetched word
-const processFetchWord = function(_word){
-	return _word.then(response => response.json())
-	.then(data => {
-		_word = data[0];
-		//_word = "bushwhacker";
-		//console.log(window.word);
-		fetchDefinition(_word).then((value) => {
-			//console.log(value);
-			if(value === 404){
-				fetchWord = fetch(wordURL);
-				fetchWord.then(processFetchWord(fetchWord));
-			} else {
-				window.word = _word;
-				console.log(dictionaryEntry);
-				//startBtn.style.display = "block";
-				addDictionaryEntryToPage()
-				for(var i = 0; i < _word.length; i++){
-					let letterElement = document.createElement("span");
-					//letterElement.innerHTML = word.charAt(i);
-					letterElement.setAttribute("id", "letter-" + i);
-					letterElement.classList.add("letter");
-					letterElement.classList.add("unguessed");
-					wordElement.appendChild(letterElement);
-				}
-			}
-		});
-	});
-}
-
-// get definition for the word
-const fetchDefinition = function(word){
-	//word = "bathyal";
-	let definitionURL = "https://owlbot.info/api/v4/dictionary/" + word;
-	return fetch(definitionURL, {
-	method: 'GET', 
-	headers: {
-		'Authorization': 'Token 8cf2d442d2de9a57913b4bee0c72692545b046df',
-	}
-}).then(response => {
-	console.log(response.status);
-	if(response.status === 404){
-		return response.status;
-		//resolve('Fail!');
-	} else {
-		return response.json();
-	}
-})
-  .then(data => {
-  	//console.log(data);
-  	dictionaryEntry = data;
-  	return data;
-  });
-} 
-
-function addDictionaryEntryToPage(){
-	const definitionsDiv = document.querySelector("#definitions-div");
-	definitionsDiv.innerHTML = "";
-	if(dictionaryEntry.pronunciation){
-		const pronunciationTitle = document.createElement("h3");
-		pronunciationTitle.innerHTML = "Pronunciation";
-		definitionsDiv.appendChild(pronunciationTitle);
-		const pronunciation = document.createElement("p");
-		pronunciation.innerHTML = dictionaryEntry.pronunciation;
-		definitionsDiv.appendChild(pronunciation);
-	}
-	if(dictionaryEntry.definitions.length > 0){
-		const definitionsTitle = document.createElement("h3");
-		(dictionaryEntry.definitions.length > 1) ? definitionsTitle.innerHTML = "Definitions" : definitionsTitle.innerHTML = "Definition";
-		definitionsDiv.appendChild(definitionsTitle);
-		const ul = document.createElement("ul");
-		for(var i = 0; i < dictionaryEntry.definitions.length; i++){
-			const li = document.createElement("li");
-			const h4 = document.createElement("h4");
-			h4.innerHTML = dictionaryEntry.definitions[i].type;
-			li.appendChild(h4);
-			const pDefinition = document.createElement("p");
-			pDefinition.classList.add("definition");
-			pDefinition.innerHTML = dictionaryEntry.definitions[i].definition;
-			li.appendChild(pDefinition);
-			if(dictionaryEntry.definitions[i].example){
-				const pExample = document.createElement("p");
-				pExample.classList.add("example");
-				pExample.innerHTML = dictionaryEntry.definitions[i].example;
-				li.appendChild(pExample);
-			}
-			ul.appendChild(li);
-		}
-		definitionsDiv.appendChild(ul);
-	}
-}
-
-
-fetchWord.then(processFetchWord(fetchWord));
+//fetchWord.then(processFetchWord(fetchWord));
 
 var guessedLetters = [];
 const guessedLettersElement = document.querySelector(".guessed-letters");
@@ -122,12 +29,29 @@ function guessLetter(event){
 				let guessedLetterElement = document.createElement("span");
 				guessedLetterElement.setAttribute("id", "guess-" + letter);
 				guessedLetterElement.classList.add("letter");
+				guessedLetterElement.classList.add("guessed");
 				guessedLetterElement.innerHTML = letter;
 				guessedLettersElement.appendChild(guessedLetterElement);
 			}
 		}
 	} else {
 		console.log("not ready");
+	}
+}
+
+function highlightLetter(event){
+	if(isLetter(event.key)){
+		const letter = event.key.toLowerCase();
+		//document.querySelector("#letter-" + key);
+		const letterElement = document.querySelector("#guess-" + letter);
+		try {
+			letterElement.classList.remove("already");
+			// trigger reflow to hack css into restarting animation
+			void letterElement.offsetWidth;
+			letterElement.classList.add("already");
+		} catch(error){
+			//console.log(error);
+		}
 	}
 }
 
@@ -145,3 +69,4 @@ function isLetter(string) {
 }
 
 document.addEventListener("keydown", guessLetter);
+document.addEventListener("keydown", highlightLetter);
